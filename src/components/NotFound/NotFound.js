77024ee4cc4b754d17406d1c5f868e2aed26c094
Your Name <you@example.com>
2021-03-11 21:14:56 +0600

    all done

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import './NotFound.css';
-import img from '../../sports-mania-main/Leauge Logo/image 2.png';
+
 
 const NotFound = () => {
     return (
-        <div className='container'>
-            <div className='row'>
-                <div className='hello d-flex align-items-center justify-content-center'>
-                    <div className="col-md-4 card-container">
-                        <div className="cards ">
-                            <figure className="front">
-                                <img src={img} alt="" />
-                                <p>name</p>
-                                <p>details</p>
-                            </figure>
-                            <figure className="back"><button>hello</button></figure>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <>
+            <h1 className="text-danger text-center">Sorry, page not found</h1>
+        </>
     );
 };
 
